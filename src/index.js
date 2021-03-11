75e3630e6c69cbcd0f5600b7eafa01ce35172706
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-03-11 17:56:19 +0300

    init: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let str1 = str.split('');
+  
+  for(let i = 0; i < str1.length; i++){
+	 
+	for(let j = 0; j < bracketsConfig.length; j++){
+		if( str1[i] === bracketsConfig[j][0] && str1[i + 1] === bracketsConfig[j][1]){
+			str1.splice(i, 2);
+		  
+		    i = -1;
+			 
+            }	
+		}	
+	} 
+  
+  if(str1.length !== 0 )return false;
+   else return true;
 }
